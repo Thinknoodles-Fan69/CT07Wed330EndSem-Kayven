11ceadb393f4717656a547f1d045de677a7bd951
Thinknoodles2012Fan <Kayvengst@gmail.com>
2025-10-22 08:16:53 +0000

    22/10/2025

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let bird
+let background, birdImg;
 
+function preload(){
+    // write code to preload bird.png and background.png
+    // refer to assets folder
+    birdImg = loadImg("assets/bird.png");
+    background = loadImg("assets/background.png");
+}
+
+function setup(){
+    new Canvas(400, 600);
+    // write code to create sprite and load bird image on sprite
+    bird = new Sprite(width / 2, height / 2, 30);
+    bird.img = birdImg;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+    
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,24 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+    world.gravity.y = 10;
+    // code to create a new square sprite
+    square = new Sprite(width / 2, height / 2, 30, 30);
+    square.collider = "dynamic" ;
+
+
+
+}
+
+function draw(){
+    background("grey");
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+        if (mouse.presses()) {
+            square.velocity.y = -5 ;
+        }
+}
